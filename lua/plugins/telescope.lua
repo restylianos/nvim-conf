72672b86cb7ef00ca6f8c1e47199cddb1b8c370f
lizzli <restemstelios@yahoo.com>
2024-04-05 19:15:34 +0300

    Initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	-- or                              , branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<C-S-F>", builtin.live_grep, {})
+	end,
+}
